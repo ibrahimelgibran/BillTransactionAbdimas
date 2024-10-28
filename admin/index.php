15bf933b5671fdf044197e4571e03e0b8884834e
ElGibran17 <ibrahimelgibran17@gmail.com>
2024-10-29 00:00:10 +0700

    update

--- admin/index.php
+++ admin/index.php
@@ -43,7 +43,7 @@ if (!isset($_SESSION["login_type"])) {
 
   <body>
     <!-- ============================================================== -->
-    <!-- Preloader - style you can find in swspinners.css -->
+    <!-- Preloader - style you can find in sswspinners.css -->
     <!-- ============================================================== -->
     <div class="preloader">
       <div class="lds-ripple">
